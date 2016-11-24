4373fda6a2a8e8986cd00fb7280c5dc6c3e015d3
QiuZiHao <qiuzihao19960303@163.com>
2016-11-24 16:38:15 +0800

    add youdao-translation

---
+++ src/youdaoFanyiAPI/YoudaoTranslate.java
@@ -0,0 +1,71 @@
+package youdaoFanyiAPI;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class YoudaoTranslate {
+
+    public static void main(String[] args) {
+        new ReadByPost().start();
+    }
+
+}
+
+class ReadByPost extends Thread{
+    @Override
+    public void run() { 
+        try {
+            URL url = new URL("http://fanyi.youdao.com/openapi.do");
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.addRequestProperty("encoding", "UTF-8");
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+
+            connection.setRequestMethod("POST");
+
+            OutputStream os = connection.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osw);
+
+            String keyword="apple";
+            
+            bw.write("keyfrom=fadabvaa&key=522071532&type=data&doctype=json&version=1.1&q="+keyword);
+            bw.flush();
+
+            InputStream is = connection.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is,"UTF-8");
+            BufferedReader br = new BufferedReader(isr);
+
+            String line;
+            StringBuilder builder = new StringBuilder();
+            while((line = br.readLine()) != null){
+                builder.append(line);
+            }
+
+            bw.close();
+            osw.close();
+            os.close();
+
+            br.close();
+            isr.close();
+            is.close();
+ 
+            int explains_index=builder.toString().indexOf("explains")+12;
+            int end_index=builder.toString().indexOf("query")-5;
+            System.out.println(builder.toString().substring(explains_index,end_index));
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
